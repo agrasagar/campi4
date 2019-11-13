2dfd792d49d632230f49566da165e408b996e101
agrasagar <agrasagar@gmail.com>
2019-11-13 23:56:03 +0200

    updated error_basic html page

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import argparse
 from flask import Flask
 from flask import Response, request, make_response, send_file, render_template, redirect
-from flask_restplus import Api
 
 from cam import Cam
 from swagger import initialize_swagger
+
+APP_IP=''
+APP_PORT=''
 
 app = Flask(__name__)
 initialize_swagger(app)
@@ -15,6 +17,8 @@ def index():
 
 @app.route('/snap/<cam_number>', methods=['GET'])
 def get_snap(cam_number):
+    global APP_IP
+    global APP_PORT
     cam = Cam("mecCam")
     colorSpace = request.args.get('colorSpace'
                                 , default="RGB"
@@ -23,7 +27,9 @@ def get_snap(cam_number):
     if colorSpace not in cam.supported_colorspace:
         return render_template('errors_basic.html'
                             , code=400
-                            , error="colorSpace {} is not supported".format(colorSpace) )
+                            , error="colorSpace {} is not supported".format(colorSpace)
+                            , ip=APP_IP
+                            , port=APP_PORT )
     status, retStr = cam.take_snap(cam_number, colorSpace)
     if status == "SUCCESS":
         image_filename = retStr
@@ -31,19 +37,29 @@ def get_snap(cam_number):
                                             , default=image_filename
                                             , type = str)
         if ".jpg" not in attachment_name[-4:]:
-            return Response(response="Only .jpg image can be expected."
-                        , status=400)
-        response = make_response(send_file(image_filename
-                                        , as_attachment=True
-                                        , attachment_filename=attachment_name))
+            error_str="Only .jpg image can be expected."
+            error_code = 400
+            response = make_response(render_template('errors_basic.html'
+                            , error=error_str, ip=APP_IP, port=APP_PORT), error_code)
+        else:
+            response = make_response(send_file( image_filename
+                                    , as_attachment=True
+                                    , attachment_filename=attachment_name), 200)
     else:
         if "Cannot identify" in retStr:
-            hw_miss_resp = "Camera is not on PI port {}.".format(cam_number)
-            response = Response(response=hw_miss_resp, status=512)
+            error_str="Camera is not on PI port {}.".format(cam_number)
+            error_code = 512
         elif "PERMISSION DENIED" in retStr.upper():
-            response = Response(response="Enable Camera in PI conig.", status=512)
+            error_str="Enable Camera in PI conig."
+            error_code = 512
+        elif "SystemError" in retStr:
+            error_str="dev id is wrong."
+            error_code=512
         else:
-            response = Response(response=retStr, status=500)
+            error_str = retStr
+            error_code = 500
+        rendered = render_template('errors_basic.html', error=error_str, ip=APP_IP, port=APP_PORT)
+        response = make_response(rendered, error_code)
     return response
 
 
@@ -52,4 +68,6 @@ if __name__ == '__main__':
     ap.add_argument("-i", help="ip for the app", default="0.0.0.0", type=str)
     ap.add_argument("-p",  help= "port for the app", default=5994, type=int)
     args = vars(ap.parse_args())
-    app.run(debug=True, port=int(args['p']), host=args['i'])
+    APP_IP = args['i']
+    APP_PORT= int(args['p'])
+    app.run(debug=True, port=APP_PORT, host=APP_IP)
